7ee2751b355f5940ddf349ec763cb35fe5d7e790
Isa Nobu <isanobu1@gmail.com>
2024-06-04 20:10:12 +0600

    footer section done

--- src/layouts/root.jsx
+++ src/layouts/root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../pages/navigation bar/NavBar";
+import Footer from "../pages/footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <NavBar />
       <Outlet />
+      <Footer></Footer>
     </div>
   );
 };
